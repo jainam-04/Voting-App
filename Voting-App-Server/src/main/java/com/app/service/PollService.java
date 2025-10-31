40d6bc70cab878b03d93f4e8d7df55400d79f2e6
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-31 18:25:28 +0530

    Created repository for poll

--- Voting-App-Server/src/main/java/com/app/service/PollService.java
+++ Voting-App-Server/src/main/java/com/app/service/PollService.java
@@ -1,8 +1,17 @@
 package com.app.service;
 
+import com.app.model.Poll;
+import com.app.repository.PollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PollService {
 
+    @Autowired
+    private PollRepository repository;
+
+    public Poll createPoll(Poll poll){
+        return repository.save(poll);
+    }
 }
