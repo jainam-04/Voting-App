1c809da4998a69b01b40b16b83bea147f6007384
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-31 18:06:42 +0530

    Created Poll model

---
+++ Voting-App-Server/src/main/java/com/app/model/Poll.java
@@ -0,0 +1,23 @@
+package com.app.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String question;
+
+    @ElementCollection
+    private List<String> options = new ArrayList<>();
+
+    @ElementCollection
+    private List<Long> votes = new ArrayList<>();
+}
