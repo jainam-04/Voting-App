ca74c98abb5bd1fbb784ce6ea36708f9a2931259
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-31 18:19:40 +0530

    Created service for poll

--- Voting-App-Server/src/main/java/com/app/controller/PollController.java
+++ Voting-App-Server/src/main/java/com/app/controller/PollController.java
@@ -1,6 +1,8 @@
 package com.app.controller;
 
 import com.app.model.Poll;
+import com.app.service.PollService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/polls")
 public class PollController {
+
+    @Autowired
+    private PollService service;
 
     @PostMapping
     public Poll createPoll(@RequestBody Poll poll){
