c386f721e75e164744b0fc7808d72f3074074ead
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-31 20:27:23 +0530

    Created get mapping to get all polls

--- Voting-App-Server/src/main/java/com/app/controller/PollController.java
+++ Voting-App-Server/src/main/java/com/app/controller/PollController.java
@@ -3,10 +3,9 @@ package com.app.controller;
 import com.app.model.Poll;
 import com.app.service.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/polls")
@@ -18,5 +17,10 @@ public class PollController {
     @PostMapping
     public Poll createPoll(@RequestBody Poll poll){
         return service.createPoll(poll);
+    }
+
+    @GetMapping
+    public List<Poll> getAllPolls(){
+        return service.getAllPolls();
     }
 }
--- Voting-App-Server/src/main/java/com/app/service/PollService.java
+++ Voting-App-Server/src/main/java/com/app/service/PollService.java
@@ -5,6 +5,8 @@ import com.app.repository.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PollService {
 
@@ -13,5 +15,9 @@ public class PollService {
 
     public Poll createPoll(Poll poll){
         return repository.save(poll);
+    }
+
+    public List<Poll> getAllPolls(){
+        return repository.findAll();
     }
 }
