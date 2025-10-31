8e4a50760ebab398921ec1593e01de75d9709fa0
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-31 18:15:09 +0530

    Created post mapping to create a poll

--- Voting-App-Server/src/main/java/com/app/controller/PollController.java
+++ Voting-App-Server/src/main/java/com/app/controller/PollController.java
@@ -1,10 +1,17 @@
 package com.app.controller;
 
+import com.app.model.Poll;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/polls")
 public class PollController {
-    
+
+    @PostMapping
+    public Poll createPoll(@RequestBody Poll poll){
+        return service.createPoll(poll);
+    }
 }
