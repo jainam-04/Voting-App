4a7cabb1b8ed22a02055a1ed5aa85c3107954476
Jainam Rupani <jainamrupani04@gmail.com>
2025-11-01 19:26:39 +0530

    Created post mapping to create poll in frontend

--- Voting-App-Frontend/src/app/poll.ts
+++ Voting-App-Frontend/src/app/poll.ts
@@ -1,11 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Poll } from './poll.models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Poll {
+export class PollService {
+  private baseUrl = "http://localhost:8080/api/polls"
+
   constructor(private http: HttpClient){}
 
-  
+  createPoll(poll: Poll): Observable<Poll>{
+    return this.http.post<Poll>(this.baseUrl, poll);
+  }
 }
